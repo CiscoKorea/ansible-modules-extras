81b51460de1fb21fdc1b373fb15e12ce072c4d68
Hyung-Soo Kim <javaos74@gmail.com>
2016-09-12 23:53:09 +0900

    bug fix when same keys exist

--- network/lldp.py
+++ network/lldp.py
@@ -66,7 +66,8 @@ def gather_lldp():
             for path_component in path_components:
                 current_dict[path_component] = current_dict.get(path_component, {})
                 current_dict = current_dict[path_component]
-            current_dict[final] = value
+            if type(current_dict) == dict:
+                current_dict[final] = value
         return output_dict
             
 
